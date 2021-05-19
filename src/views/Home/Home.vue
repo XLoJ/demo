bcc36bd238c8c7d8f4f751f645ee04e052663671
Qi You <1425321705@qq.com>
2021-05-19 18:36:03 +0800

    Update Home.vue

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,7 +3,177 @@
     <div>
       <Navbar></Navbar>
     </div>
-    <div class="container"></div>
+    <div class="container">
+      <div class="columns">
+        <div class="column is-two-thirds">
+          <h4 class="title is-4">南京理工大学</h4>
+          <h5 class="title is-5">OJ评测系统</h5>
+          <!-- <h2 class="subtitle is-3">Powered By ...</h2> -->
+          <br />
+          <div class="box">
+            <article class="media">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img src="https://bulma.io/images/placeholders/128x128.png" />
+                </p>
+              </figure>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>余立功老师</strong> <small>@鱼头</small>
+                    <br />
+                    那我们就起个名儿，叫“赛码极客工坊”<br />
+                    大家觉得如何？
+                  </p>
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-reply"></i
+                      ></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-retweet"></i
+                      ></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-heart"></i
+                      ></span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </article>
+          </div>
+          <div class="box">
+            <article class="media">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img src="https://bulma.io/images/placeholders/128x128.png" />
+                </p>
+              </figure>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>余立功老师</strong> <small>@鱼头</small>
+                    <br />
+                    请各位队员在11号之前在群中汇报上一个月的训练情况<br />
+                    明天下午三点在活动室开会，大家务必记得到场
+                  </p>
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-reply"></i
+                      ></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-retweet"></i
+                      ></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-heart"></i
+                      ></span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </article>
+          </div>
+          <div class="box">
+            <article class="media">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img src="https://bulma.io/images/placeholders/128x128.png" />
+                </p>
+              </figure>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong>颜俊梁</strong> <small>@跃迁引擎启动</small>
+                    <br />
+                    好耶！
+                  </p>
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-reply"></i
+                      ></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-retweet"></i
+                      ></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"
+                        ><i class="fas fa-heart"></i
+                      ></span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">公告牌</p>
+              <p class="subtitle">从这里知道你想要的...</p>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card">
+                <div class="card-content">
+                  <div class="content">
+                    欢迎来到南京理工大学OJ论坛！<br />
+                    南京理工大学计算机科学与工程学院始建于1953年创办的哈尔滨军事工程学院模拟计算机研究组，先后经历了炮兵工程学院计算机教研室(1960年)、华东工程学院计算机科学与工程系（1979年）等发展阶段，是我国高等学校中较早建立的计算机系之一。2005年12月更名为计算机科学与技术学院，2012年5月改为现名。2018年6月学校批准成立人工智能学院，与计算机科学与工程学院合署运行。学院下设智能科学与技术系、计算机科学与技术系、软件工程系、数据科学与工程系、网络空间安全系、教育部重点实验室、智能机器人研究所等二级单位。<br />
+                    ICPC（英文：International Collegiate Programming
+                    Contest，中文：国际大学生程序设计竞赛）<br />
+                    由 ICPC 基金会（英文：ICPC
+                    Foundation）举办，是最具影响力的大学生计算机竞赛。由于以前
+                    ACM 赞助这个竞赛，也有很多人习惯叫它 ACM 竞赛。 ICPC<br />
+                    主要分为区域赛（Regionals）和总决赛（World
+                    Finals）两部分。<br />
+                    官网地址：https://icpc.global<br />
+                    官网地址：https://ccpc.io 中国大学生程序设计竞赛。 和 ICPC
+                    显著的区别是很多学校是不报销的。
+                  </div>
+                </div>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <p class="card-footer-item">
+                <span>
+                  加入
+                  <a
+                    href="https://twitter.com/codinghorror/status/506010907021828096"
+                    >爱好者协会</a
+                  >
+                </span>
+              </p>
+              <p class="card-footer-item">
+                <span> 前往 <a href="#">竞赛论坛</a> </span>
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
