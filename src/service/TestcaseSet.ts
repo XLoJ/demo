eaee5c88338d27dbe74c9b4177e5d797f4a93e4b
XLor <yjl9903@vip.qq.com>
2021-05-22 01:20:00 +0800

    add: edit testcases

---
+++ src/service/TestcaseSet.ts
@@ -0,0 +1,134 @@
+import { api } from '@/service/api';
+
+export type TestcaseItem =
+  | {
+      type: 'file';
+      filename: string;
+      example?: boolean;
+    }
+  | {
+      type: 'generator';
+      generator: number;
+      args: string[];
+      example?: boolean;
+    };
+
+export type StaticFileItem = {
+  filename: string;
+  body: string;
+};
+
+export type GeneratorItem = {
+  id: number;
+  version: number;
+  name: string;
+  type: string;
+  body: string;
+  language: string;
+};
+
+export class TestcaseSet {
+  readonly pid: number;
+  readonly testcases: TestcaseItem[];
+  readonly staticFiles: StaticFileItem[];
+  readonly generators: GeneratorItem[];
+
+  constructor(
+    pid: number,
+    testcases: TestcaseItem[],
+    staticFiles: StaticFileItem[],
+    generators: GeneratorItem[]
+  ) {
+    this.pid = pid;
+    this.testcases = testcases;
+    this.staticFiles = staticFiles;
+    this.generators = generators;
+  }
+
+  async addFromStaticFile(filename: string) {
+    if (this.staticFiles.findIndex((f) => f.filename === filename) === -1) {
+      throw new Error(`静态文件 ${filename} 不存在`);
+    }
+    if (
+      this.testcases.findIndex(
+        (item) => item.type === 'file' && item.filename === filename
+      ) === -1
+    ) {
+      this.testcases.push({
+        type: 'file',
+        filename,
+        example: false
+      });
+      try {
+        await api.post(`/polygon/problem/${this.pid}/testcases`, {
+          testcases: JSON.stringify(this.testcases)
+        });
+      } catch (err) {
+        this.testcases.pop();
+        throw err;
+      }
+    } else {
+      throw new Error(`${filename} 已经添加到测试数据中`);
+    }
+  }
+
+  async addFromGenerator(gid: number, args: string[]) {
+    if (this.generators.findIndex((g) => g.id === gid) === -1) {
+      throw new Error(`Generator ${gid} 不存在`);
+    }
+    if (
+      this.testcases.findIndex(
+        (item) =>
+          item.type === 'generator' &&
+          item.generator === gid &&
+          item.args.join(' ') === args.join(' ')
+      ) === -1
+    ) {
+      this.testcases.push({
+        type: 'generator',
+        generator: gid,
+        args,
+        example: false
+      });
+      try {
+        await api.post(`/polygon/problem/${this.pid}/testcases`, {
+          testcases: JSON.stringify(this.testcases)
+        });
+      } catch (err) {
+        this.testcases.pop();
+        throw err;
+      }
+    } else {
+      throw new Error(`生成重复数据`);
+    }
+  }
+
+  async removeTestcase(index: number) {
+    const oldItem = this.testcases.splice(index, 1);
+    try {
+      await api.post(`/polygon/problem/${this.pid}/testcases`, {
+        testcases: JSON.stringify(this.testcases)
+      });
+    } catch (err) {
+      this.testcases.splice(index, 0, ...oldItem);
+      throw err;
+    }
+  }
+
+  async setExample(index: number) {
+    if (index < 0 || index >= this.testcases.length) {
+      throw new Error(`测试数据 ${index} 已失效`);
+    }
+    const old = this.testcases[index].example;
+    this.testcases[index].example = !old;
+    try {
+      await api.post(`/polygon/problem/${this.pid}/testcases`, {
+        testcases: JSON.stringify(this.testcases)
+      });
+    } catch (err) {
+      this.testcases[index].example = old;
+      throw err;
+    }
+    return !old;
+  }
+}
--- src/views/Polygon/EditTestcases.vue
+++ src/views/Polygon/EditTestcases.vue
@@ -1,66 +1,96 @@
 <template>
   <div>
     <div class="mb-2 is-flex is-align-items-center">
-      <span style="width: 11em" class="has-text-weight-bold is-inline-block"
+      <span class="has-text-weight-bold is-inline-block" style="width: 11em"
         >从静态文件生成：</span
       >
       <b-select
-        style="width: 10em"
-        expanded
+        v-model="selectStaticFile"
         class="is-inline-block"
-        v-model="lang"
+        expanded
         placeholder="选择静态文件"
+        style="width: 10em"
       >
         <option
           v-for="(file, index) in staticFiles"
-          :value="index"
           :key="file.filename"
+          :value="index"
         >
           {{ file.filename }}
         </option>
       </b-select>
-      <b-button class="ml-4" type="is-success">添加</b-button>
+      <b-button class="ml-4" type="is-success" @click="addFromStaticFile"
+        >添加</b-button
+      >
     </div>
+
     <div class="mb-2 is-flex is-align-items-center">
-      <span style="width: 11em" class="has-text-weight-bold is-inline-block"
+      <span class="has-text-weight-bold is-inline-block" style="width: 11em"
         >使用 Generator 生成：</span
       >
       <b-select
-        style="width: 10em"
-        expanded
+        v-model="selectGenerator"
         class="is-inline-block mr-4"
-        v-model="lang"
+        expanded
         placeholder="选择 Generator"
+        style="width: 10em"
       >
         <option
-          style="width: 10em"
           v-for="(generator, index) in generators"
-          :value="index"
           :key="index"
+          :value="index"
+          style="width: 10em"
         >
           {{ generator.name }}.{{ generator.language }}
         </option>
       </b-select>
-      <b-input placeholder="运行参数"></b-input>
-      <b-button class="ml-4" type="is-success">添加</b-button>
+      <b-input v-model="generatorArgs" placeholder="运行参数"></b-input>
+      <b-button class="ml-4" type="is-success" @click="addFromGenerator"
+        >添加</b-button
+      >
     </div>
+
     <b-table :data="testcases">
-      <b-table-column label="#" v-slot="props" width="24">
-        <span class="has-text-weight-bold">{{ props.index }}</span>
+      <b-table-column v-slot="props" label="#" width="24">
+        <span class="has-text-weight-bold">{{ props.index + 1 }}</span>
       </b-table-column>
-      <b-table-column label="测试用例" v-slot="props">
-        <span>{{ props.index }}</span>
+      <b-table-column v-slot="props" label="测试用例">
+        <div v-if="props.row.type === 'file'">
+          <span>静态文件：</span>
+          <b-tag>{{ props.row.filename }}</b-tag>
+        </div>
+        <div v-else>
+          <span>Generator：</span>
+          <b-tag>{{ getGeneratorName(props.row.generator) }}</b-tag>
+          <b-tag
+            v-for="(item, index) in props.row.args"
+            :key="index"
+            class="ml-2"
+            >{{ item }}</b-tag
+          >
+        </div>
       </b-table-column>
-      <b-table-column label="样例？" width="80" v-slot="props">
-        <span>{{ props.index }}</span>
+      <b-table-column v-slot="props" centered label="样例？" width="8em">
+        <span class="icon">
+          <i v-if="props.row.example" class="mdi mdi-check"></i>
+        </span>
+        <b-button
+          class="ml-1"
+          size="is-small"
+          type="is-text"
+          @click="setExample(props.index)"
+        >
+          <span v-if="props.row.example">取消</span>
+          <span v-else>设置</span>
+        </b-button>
       </b-table-column>
       <b-table-column v-slot="props" centered label="操作" width="80">
-        <span class="is-inline-block pt-2">
+        <span class="is-inline-block">
           <b-button
             icon-left="delete"
             size="is-small"
             type="is-danger"
-            @click="remove(props.index)"
+            @click="removeTestcase(props.index)"
           ></b-button>
         </span>
       </b-table-column>
@@ -69,8 +99,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@vue/composition-api';
+import { defineComponent, reactive, ref } from '@vue/composition-api';
 import { getAllStaticFilename } from '@/service/polygon';
+import { TestcaseSet } from '@/service/TestcaseSet';
+import { useSnackbar } from '@/utils';
 
 export default defineComponent({
   name: 'EditTestcases',
@@ -86,12 +118,97 @@ export default defineComponent({
       staticFiles.push(...data);
     });
 
-    const testcases = reactive([]);
+    const testcases = reactive(JSON.parse(problem.testcases));
+    const testcaseSet = new TestcaseSet(
+      problem.parent,
+      testcases,
+      staticFiles,
+      generators
+    );
+
+    const snackbar = useSnackbar();
+
+    const selectStaticFile = ref(0);
+    const addFromStaticFile = async () => {
+      try {
+        const filename = staticFiles[selectStaticFile.value].filename;
+        await testcaseSet.addFromStaticFile(filename);
+        snackbar.open(`静态文件 ${filename} 已经成功添加`);
+      } catch (err) {
+        snackbar.open({
+          message: err.message,
+          type: 'is-danger'
+        });
+      }
+    };
+
+    const selectGenerator = ref(0);
+    const generatorArgs = ref('');
+    const getGeneratorName = (gid: number) => {
+      for (const g of generators) {
+        if (g.id === gid) {
+          return `${g.name}.${g.language}`;
+        }
+      }
+      // Unknown
+      return 'Unknown';
+    };
+    const addFromGenerator = async () => {
+      try {
+        const generator = generators[selectGenerator.value];
+        const args = generatorArgs.value
+          .split(' ')
+          .map((s) => s.trim())
+          .filter((s) => s.length > 0);
+        await testcaseSet.addFromGenerator(generator.id, args);
+        snackbar.open(`添加成功`);
+      } catch (err) {
+        snackbar.open({
+          message: err.message,
+          type: 'is-danger'
+        });
+      }
+    };
+
+    const removeTestcase = async (index: number) => {
+      try {
+        await testcaseSet.removeTestcase(index);
+        snackbar.open(`删除成功`);
+      } catch (err) {
+        snackbar.open({
+          message: err.message,
+          type: 'is-danger'
+        });
+      }
+    };
+    const setExample = async (index: number) => {
+      try {
+        const flag = await testcaseSet.setExample(index);
+        if (flag) {
+          snackbar.open(`测试数据 ${index} 已被设置为样例`);
+        } else {
+          snackbar.open(`测试数据 ${index} 已取消成为样例`);
+        }
+      } catch (err) {
+        snackbar.open({
+          message: err.message,
+          type: 'is-danger'
+        });
+      }
+    };
 
     return {
       generators,
       staticFiles,
-      testcases
+      testcases,
+      selectStaticFile,
+      selectGenerator,
+      generatorArgs,
+      getGeneratorName,
+      addFromStaticFile,
+      addFromGenerator,
+      removeTestcase,
+      setExample
     };
   }
 });
